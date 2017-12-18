e956499a27169ac6a0c537c4cc3e6b78090de99b
Kahel3352 <micaoa@gmail.com>
2017-12-18 16:23:54 +0100

    add Vérification paramètres

---
+++ databaseToXmlMea.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-12-18T16:09:43
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = databaseToXmlMea
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Vérifie les paramètres passés au programme
+ * @return true si les paramètres sont bons
+ */
+bool checkArgs();
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    if(arc)
+    cout<<argv[1]<<endl;
+    return 0;
+}
+
+bool checkArgs()
+{
+
+}
